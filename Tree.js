0f3d1b0159a9fa5cb645b8f3e3bb9c7435929c58
ZakChebaane <150349051+ZakChebaane@users.noreply.github.com>
2026-08-11 10:50:52 +0100

    tree works for when the array splits down to a aingle [element]

--- Tree.js
+++ Tree.js
@@ -3,6 +3,7 @@ import {Node} from "./Node.js";
 export class Tree {
     constructor(passedArr) {
         this.root = this.buildTree(passedArr);
+        console.log(this.root);
     }
     buildTree(arr) {
         // sort the arr and remove duplicates
@@ -14,7 +15,25 @@ export class Tree {
                 return element;
             }
         });
-        
-        return arr;
+        // recursion start
+        if (arr.length <= 2) {
+            // Base Case
+            if (arr.length === 1) {
+                return arr[0];
+            } else if (arr.length === 2) {
+                console.log("haven't done this bit yet :(");
+                return undefined;
+            }
+        } else if (arr.length > 2) {
+            const middle = (arr.length - 1) / 2;
+            const rootValue = arr[middle];
+            const leftNodes = this.buildTree(arr.slice(0, middle));
+            const rightNodes = this.buildTree(arr.slice(middle + 1));
+            const node = new Node();
+            node.storedData = rootValue;
+            node.leftChild = leftNodes;
+            node.rightChild = rightNodes;
+            return node;
+        }
     }
 }
