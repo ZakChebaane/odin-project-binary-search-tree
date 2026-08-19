b6539f6772488d45252b02b5e335b9cebc0fd65b
ZakChebaane <150349051+ZakChebaane@users.noreply.github.com>
2026-08-19 13:16:45 +0100

    Binary Search Tree Project tested and completed

--- index.js
+++ index.js
@@ -1,23 +1,59 @@
 import {Tree} from "./Tree.js";
 
-// const myBST = new Tree([1,5,2,1,4,6,7,3,8]);
-const myBST = new Tree([4]);
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+const arrBelow100 = [];
+const arrAbove100 = [];
+for(let i = 0; i < 10; i++) {
+    arrBelow100.push(getRandomInt(99));
+    let above100 = getRandomInt(200);
+    let above = false;
+    while (!above) {
+        if(above100 > 100) {
+            above = true;
+        } else {
+            above = getRandomInt(200);
+        }
+    }
+    arrAbove100.push(above100);
+}
 
-// myBST.insert(6.5);
-
-myBST.insert(3);
-myBST.insert(2);
-myBST.insert(1);
-myBST.insert(5);
-myBST.insert(6);
-myBST.insert(7);
-myBST.insert(8);
+const myBST = new Tree(arrBelow100);
 
 myBST.prettyPrint();
-
-// console.log(myBST.height(6));
 console.log(myBST.isBalanced());
+myBST.levelOrderForEach((el) => {
+    console.log(el)
+});
+myBST.preOrderForEach((el) => {
+    console.log(el)
+});
+myBST.postOrderForEach((el) => {
+    console.log(el)
+});
+myBST.inOrderForEach((el) => {
+    console.log(el)
+});
 
+for(let i = 0; i < arrAbove100.length; i++) {
+    myBST.insert(arrAbove100[i]);
+}
+myBST.prettyPrint();
+console.log(myBST.isBalanced());
 myBST.rebalance();
 myBST.prettyPrint();
 console.log(myBST.isBalanced());
+
+myBST.levelOrderForEach((el) => {
+    console.log(el)
+});
+myBST.preOrderForEach((el) => {
+    console.log(el)
+});
+myBST.postOrderForEach((el) => {
+    console.log(el)
+});
+myBST.inOrderForEach((el) => {
+    console.log(el)
+});
